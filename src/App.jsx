4b114ec10b2e4947ae41ece83e6fdc8b88fdb610
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-14 19:44:49 +0530

    Added Time

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date())
 
+  const addZero = (time)=>{
+    return time<10 ? `0${time}` : time
+  }
+  const handleHours = (hours)=>{
+    return hours==0 ? 12:hours>12?hours-12:hours 
+  }
+  useEffect(()=>{
+    const timer = setInterval(()=>setCurrentTime(new Date()), 1000)
+    return ()=> clearInterval(timer)
+  },[])
   return (
     <>
 <div className="digital-clock">
   <h1>Digital Clock</h1>
-  <div className="time">06:15:59 PM</div>
+
+  {/* <div className="time">{currentTime.toLocaleTimeString()}</div> */}
+  <div className="time">{addZero(handleHours(currentTime.getHours()))}:
+      {addZero(currentTime.getMinutes())}:{addZero(currentTime.getSeconds())}</div>
   <div className="date">Tuesday, February 6, 2024</div>
 </div>
     </>
