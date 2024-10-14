beeda08f8df0d220fac4e4fdf523ebe7ccf4bc5a
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-14 19:57:07 +0530

    Added Date & Meridian with time

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date())
-
+  const options = { weekday:"long",year:"numeric",month:"long",day:"numeric"} // any order
   const addZero = (time)=>{
     return time<10 ? `0${time}` : time
   }
   const handleHours = (hours)=>{
     return hours==0 ? 12:hours>12?hours-12:hours 
   }
+
   useEffect(()=>{
     const timer = setInterval(()=>setCurrentTime(new Date()), 1000)
     return ()=> clearInterval(timer)
@@ -18,11 +19,12 @@ function App() {
     <>
 <div className="digital-clock">
   <h1>Digital Clock</h1>
-
-  {/* <div className="time">{currentTime.toLocaleTimeString()}</div> */}
+{/* gives no zero for hours when single digit */}
+  {/* <div className="time">{currentTime.toLocaleTimeString()}</div> */} 
   <div className="time">{addZero(handleHours(currentTime.getHours()))}:
-      {addZero(currentTime.getMinutes())}:{addZero(currentTime.getSeconds())}</div>
-  <div className="date">Tuesday, February 6, 2024</div>
+      {addZero(currentTime.getMinutes())}:{addZero(currentTime.getSeconds())} 
+      {currentTime.getHours()>12?" PM":" AM"}</div>
+  <div className="date">{currentTime.toLocaleDateString(undefined,options)}</div>
 </div>
     </>
   )
